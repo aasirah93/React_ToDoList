1b3dfe9275f73c0f7f6a62062f06822d1160d5f7
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-08-06 19:00:22 +0100

    Climbing the tree

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,9 +3,14 @@ import TodoItem from './Todoitem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component {
+  markComplete = () => {
+    console.log('Hello')
+  }
+
   render() {
     return this.props.todos.map((todo) => (
-      <TodoItem key={todo.id} todo={todo} />
+      <TodoItem key={todo.id} todo={todo} markComplete=
+      {this.props.markComplete} />
     ));
   }
 };
